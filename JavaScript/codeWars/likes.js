4be141f212a4cee979c56901c9258678b27c1dbf
Rifat Hossain <rh@oiiu.com>
2021-02-06 08:47:46 +0600

    added some thing

---
+++ JavaScript/codeWars/likes.js
@@ -0,0 +1,18 @@
+
+function likes(arr = []) {
+  if (arr.length === 0) {
+    return 'no one likes this';
+  }
+  if (arr.length === 1) {
+    return `${arr[0]} likes this`;
+  }
+  if (arr.length === 2) {
+    return `${arr[0]} and ${arr[1]} like this`;
+  }
+  if (arr.length === 3) {
+    return `${arr[0]}, ${arr[1]} and ${arr[2]} like this`;
+  }
+  return `${arr[0]}, ${arr[1]} and ${arr.length - 2} others like this`
+}
+
+console.log(likes());
